6f4820e5faaf29c802aec074ff32935ba8391811
defor <deforworks@gmail.com>
2019-10-31 15:00:53 +0300

    Обработчики и шаблоны URL

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,4 +5,8 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-
+def post_detail(request, year, mouth, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',       
+                            publish_year=year, publish_mouth=mouth,         
+                            publish_day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
